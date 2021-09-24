95ca275da482c66a999a3674b86ecb12532bfb81
kevin <dqtkien@gmail.com>
2021-09-24 15:56:00 +0700

    Adding Material UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { createMuiTheme } from '@mui/material';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import blue from '@material-ui/core/colors/blue';
+
 import HomePage from './page/HomePage/Home';
 
 const App = () => {
+  const theme = createMuiTheme({
+    palette: {
+      primary: blue,
+    },
+  });
   const routes = (
     <Switch>
       <Route path="/" component={HomePage} />
