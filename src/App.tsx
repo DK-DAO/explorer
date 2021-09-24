cc57a2d668441c1e72187e0a27391143c521af72
kevin <dqtkien@gmail.com>
2021-09-24 17:03:37 +0700

    Delete unused const

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,8 @@
-import { createMuiTheme } from '@mui/material';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import blue from '@material-ui/core/colors/blue';
 
 import HomePage from './page/HomePage/Home';
 
 const App = () => {
-  const theme = createMuiTheme({
-    palette: {
-      primary: blue,
-    },
-  });
   const routes = (
     <Switch>
       <Route path="/" component={HomePage} />
