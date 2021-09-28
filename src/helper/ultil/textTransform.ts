e5771fd421f5b65951194cdbefec9ac1f0db4deb
kevin <dqtkien@gmail.com>
2021-09-28 10:11:21 +0700

    Add more text transform

--- src/helper/ultil/textTransform.ts
+++ src/helper/ultil/textTransform.ts
@@ -1,6 +1,10 @@
-const camelCaseToPascalCase = (text: string) => {
+export const camelCaseToPascalCase = (text: string) => {
   const newText = text.replace(/([A-Z])/g, ' $1');
   return newText.charAt(0).toUpperCase() + newText.slice(1);
 };
 
-export default camelCaseToPascalCase;
+export const underscoreToCapitalizeWithSpace = (text: string) => {
+  return text.replace(/(?:_| |\b)(\w)/g, (s) => {
+    return s.toUpperCase().replace('_', ' ');
+  });
+};
