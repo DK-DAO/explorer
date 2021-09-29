abbf9c4b8b7d36eb6a7cf1c4b0f378c9496cb2c4
kevin <dqtkien@gmail.com>
2021-09-29 17:17:19 +0700

    Fix route issue. Add search bar. Add basic form

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -1,4 +1,14 @@
-import { AppBar, Toolbar, Typography, styled } from '@mui/material';
+import React, { useState } from 'react';
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  styled,
+  alpha,
+  InputBase,
+} from '@mui/material';
+import { Link, useHistory } from 'react-router-dom';
+import SearchIcon from '@mui/icons-material/Search';
 import logoIcon from '../asset/image/logo.png';
 
 const StyledImage = styled('img')(({ theme }) => ({
@@ -6,13 +16,85 @@ const StyledImage = styled('img')(({ theme }) => ({
   width: '50px',
 }));
 
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginRight: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(1),
+    width: 'auto',
+  },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: '12ch',
+      '&:focus': {
+        width: '20ch',
+      },
+    },
+  },
+}));
+
 const Header = () => {
+  const [text, setText] = useState<string>('');
+  const history = useHistory();
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    history.push(`/view/${text}`);
+  };
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    setText(value);
+  };
+
   return (
     <>
       <AppBar position="sticky" color="primary">
         <Toolbar>
-          <StyledImage src={logoIcon} />
-          <Typography variant="h6">DKDAO</Typography>
+          <Link to="/">
+            <StyledImage src={logoIcon} />
+          </Link>
+          <Typography variant="h6" display="block" sx={{ mr: 2, flexGrow: 1 }}>
+            DKDAO
+          </Typography>
+          <form id="searchCard" onSubmit={handleSubmit}>
+            <Search>
+              <SearchIconWrapper>
+                <SearchIcon />
+              </SearchIconWrapper>
+              <StyledInputBase
+                placeholder="Search card …"
+                inputProps={{ 'aria-label': 'search' }}
+                name="searchField"
+                onChange={handleChange}
+              />
+            </Search>
+          </form>
         </Toolbar>
       </AppBar>
     </>
